5abe463efb7873deaaa52dba4a10c8fd44316d2d
Stian Frøystein <stianfroy@gmail.com>
2021-07-22 10:54:37 +0200

    Remove all unneeded stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger!</h1>;
+}
+
+const user = {
+  firstName: "Stian",
+  lastName: "Froystein",
+  avatarUrl: "https://avatars.githubusercontent.com/u/26657174?v=4",
+};
+
+const element = (
+  <>
+    <div tabIndex="0">
+      <button>
+        {getGreeting(user)}
+        <img alt="foo" src={user.avatarUrl}></img>;
+      </button>
+    </div>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(element, document.getElementById("root"));
