94d0dd193f510e1c05bde5b5ef269d7419bd541f
Stian Frøystein <stianfroy@gmail.com>
2021-07-26 16:27:05 +0200

    Add standalone clock component

--- src/index.js
+++ src/index.js
@@ -1,32 +1,150 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function formatName(user) {
-  return user.firstName + " " + user.lastName;
-}
+// function formatName(user) {
+//   return user.firstName + " " + user.lastName;
+// }
 
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hello, {formatName(user)}!</h1>;
+// function getGreeting(user) {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)}!</h1>;
+//   }
+//   return <h1>Hello, Stranger!</h1>;
+// }
+
+// const user = {
+//   firstName: "Stian",
+//   lastName: "Froystein",
+//   avatarUrl: "https://avatars.githubusercontent.com/u/26657174?v=4",
+// };
+
+// const element = (
+//   <>
+//     <div tabIndex="0">
+//       <button>
+//         {getGreeting(user)}
+//         <img alt="foo" src={user.avatarUrl}/>;
+//       </button>
+//     </div>
+//   </>
+// );
+
+// const element = React.createElement(
+//   "h1",
+//   {className: "greeting"},
+//   "Hello, world!"
+// )
+
+// <div> id="root" </div>
+// const element = <h1>Hello, world</h1>
+// ReactDOM.render(element, document.getElementById("root"));
+
+// function Foo() {
+//   return <h2>Bar</h2>;
+// }
+
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="Stian" />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// function formatDate(date) {
+//   return date.toLocaleDateString();
+// }
+
+// function Avatar(props) {
+//   return (
+//     <img
+//       className="avatar"
+//       src={props.user.avatarUrl}
+//       alt={props.user.name}
+//       width="60px"
+//       height="auto"
+//     />
+//   );
+// }
+
+// function UserInfo(props) {
+//   return (
+//     <div className="UserInfo">
+//       <Avatar user={props.user} />
+//       <div className="UserInfo-name">{props.user.name}</div>
+//     </div>
+//   );
+// }
+
+// function Comment(props) {
+//   return (
+//     <div className="Comment">
+//       <UserInfo user={props.author} />
+//       <div className="Comment-text">{props.text}</div>
+//       <div className="Comment-date">{formatDate(props.date)}</div>
+//     </div>
+//   );
+// }
+
+// const comment = {
+//   date: new Date(),
+//   text: "I do like React.",
+//   author: {
+//     name: "Stian",
+//     avatarUrl: "https://avatars.githubusercontent.com/u/26657174?v=4",
+//   },
+// };
+
+// ReactDOM.render(
+//   <Comment date={comment.date} text={comment.text} author={comment.author} />,
+//   document.getElementById("root")
+// );
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+    };
   }
-  return <h1>Hello, Stranger!</h1>;
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
 }
 
-const user = {
-  firstName: "Stian",
-  lastName: "Froystein",
-  avatarUrl: "https://avatars.githubusercontent.com/u/26657174?v=4",
-};
-
-const element = (
-  <>
-    <div tabIndex="0">
-      <button>
-        {getGreeting(user)}
-        <img alt="foo" src={user.avatarUrl}></img>;
-      </button>
+function App(props) {
+  return (
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
     </div>
-  </>
-);
+  );
+}
 
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
