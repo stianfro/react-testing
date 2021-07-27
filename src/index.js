fea9ed0cee52671e10313592118499da1febb1e5
Stian Frøystein <stianfroy@gmail.com>
2021-07-27 14:12:12 +0200

    Working api fetch and bifrost table!

--- src/index.js
+++ src/index.js
@@ -1,185 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import useSWR from "swr";
+import { Nav } from "@intility/bifrost-react";
+import { faHome, faUser, faCog } from "@fortawesome/free-solid-svg-icons";
+import { Table } from "@intility/bifrost-react";
 
-// function formatName(user) {
-//   return user.firstName + " " + user.lastName;
-// }
+import "@intility/bifrost-react/dist/bifrost-app.css";
 
-// function getGreeting(user) {
-//   if (user) {
-//     return <h1>Hello, {formatName(user)}!</h1>;
-//   }
-//   return <h1>Hello, Stranger!</h1>;
-// }
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
-// const user = {
-//   firstName: "Stian",
-//   lastName: "Froystein",
-//   avatarUrl: "https://avatars.githubusercontent.com/u/26657174?v=4",
-// };
+function Example() {
+  const { data, error } = useSWR(
+    `http://localhost:5000/api/v1/DefenderException`,
+    fetcher
+  );
 
-// const element = (
-//   <>
-//     <div tabIndex="0">
-//       <button>
-//         {getGreeting(user)}
-//         <img alt="foo" src={user.avatarUrl}/>;
-//       </button>
-//     </div>
-//   </>
-// );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return (
+    // <pre>{JSON.stringify(data.exclusions[0].path, null, 2)}</pre>
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Server</Table.HeaderCell>
+          <Table.HeaderCell>Path</Table.HeaderCell>
+          <Table.HeaderCell>Extension</Table.HeaderCell>
+          <Table.HeaderCell>Process Path</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>{data[1].server}</Table.Cell>
+          <Table.Cell>{data[1].exclusions[0].path}</Table.Cell>
+          <Table.Cell>{data[1].exclusions[0].extension}</Table.Cell>
+          <Table.Cell>{data[1].exclusions[0].processPath}</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  );
+}
 
-// const element = React.createElement(
-//   "h1",
-//   {className: "greeting"},
-//   "Hello, world!"
-// )
+const App = () => (
+  <Nav
+    appName="Defender Portal"
+    top={
+      <>
+        <a href="/path" title="Profile">
+          <Nav.Item icon={faUser} />
+        </a>
+        <a href="/path">
+          <Nav.Item>About</Nav.Item>
+        </a>
+      </>
+    }
+    side={
+      <>
+        <a href="/path">
+          <Nav.Item icon={faHome}>Home</Nav.Item>
+        </a>
+        <a href="/path">
+          <Nav.Item icon={faCog}>Settings</Nav.Item>
+        </a>
+      </>
+    }
+  >
+    <Example />
+  </Nav>
+);
 
-// <div> id="root" </div>
-// const element = <h1>Hello, world</h1>
-// ReactDOM.render(element, document.getElementById("root"));
-
-// function Foo() {
-//   return <h2>Bar</h2>;
-// }
-
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
-// function App() {
-//   return (
-//     <div>
-//       <Welcome name="Stian" />
-//     </div>
-//   );
-// }
-
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-// function formatDate(date) {
-//   return date.toLocaleDateString();
-// }
-
-// function Avatar(props) {
-//   return (
-//     <img
-//       className="avatar"
-//       src={props.user.avatarUrl}
-//       alt={props.user.name}
-//       width="60px"
-//       height="auto"
-//     />
-//   );
-// }
-
-// function UserInfo(props) {
-//   return (
-//     <div className="UserInfo">
-//       <Avatar user={props.user} />
-//       <div className="UserInfo-name">{props.user.name}</div>
-//     </div>
-//   );
-// }
-
-// function Comment(props) {
-//   return (
-//     <div className="Comment">
-//       <UserInfo user={props.author} />
-//       <div className="Comment-text">{props.text}</div>
-//       <div className="Comment-date">{formatDate(props.date)}</div>
-//     </div>
-//   );
-// }
-
-// const comment = {
-//   date: new Date(),
-//   text: "I do like React.",
-//   author: {
-//     name: "Stian",
-//     avatarUrl: "https://avatars.githubusercontent.com/u/26657174?v=4",
-//   },
-// };
-
-// ReactDOM.render(
-//   <Comment date={comment.date} text={comment.text} author={comment.author} />,
-//   document.getElementById("root")
-// );
-
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       date: new Date(),
-//     };
-//   }
-
-//   componentDidMount() {
-//     this.timerID = setInterval(() => this.tick(), 1000);
-//   }
-
-//   componentWillUnmount() {
-//     clearInterval(this.timerID);
-//   }
-
-//   tick() {
-//     this.setState({
-//       date: new Date(),
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h2>It is {this.state.date.toLocaleTimeString()}</h2>
-//       </div>
-//     );
-//   }
-// }
-
-// function App(props) {
-//   return (
-//     <div>
-//       <Clock />
-//       <Clock />
-//       <Clock />
-//     </div>
-//   );
-// }
-
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-// function Form() {
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     console.log("You clicked submit.");
-//   }
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <button type="submit">Submit</button>
-//     </form>
-//   );
-// }
-
-// ReactDOM.render(<Form />, document.getElementById("root"));
-
-// class Toggle extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { isToggleOn: true };
-
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick() {
-//     this.setState
-//   }
-
-//   render() {
-//     return <button></button>;
-//   }
-// }
-
-// ReactDOM.render(<Toggle />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
