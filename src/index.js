d3899f8f835d1d52a03d055c19b71e328f59dff4
Stian Frøystein <stianfroy@gmail.com>
2021-07-27 15:08:17 +0200

    Simplify

--- src/index.js
+++ src/index.js
@@ -2,14 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom";
 import useSWR from "swr";
 import { Nav } from "@intility/bifrost-react";
-import { faHome, faUser, faCog } from "@fortawesome/free-solid-svg-icons";
 import { Table } from "@intility/bifrost-react";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 import "@intility/bifrost-react/dist/bifrost-app.css";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
-function Example() {
+class ExceptionTable extends React.Component {
+  renderRow(props) {
+    return (
+      <Table.Row>
+        <Table.Cell>{props.server}</Table.Cell>
+        <Table.Cell>{props.exclusions[0].path}</Table.Cell>
+        <Table.Cell>{props.exclusions[0].extension}</Table.Cell>
+        <Table.Cell>{props.exclusions[0].processPath}</Table.Cell>
+      </Table.Row>
+    );
+  }
+
+  render() {
+    return (
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Server</Table.HeaderCell>
+            <Table.HeaderCell>Path</Table.HeaderCell>
+            <Table.HeaderCell>Extension</Table.HeaderCell>
+            <Table.HeaderCell>Process Path</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>{this.props.rows.map(this.renderRow)}</Table.Body>
+      </Table>
+    );
+  }
+}
+
+function DefenderOverview() {
   const { data, error } = useSWR(
     `http://localhost:5000/api/v1/DefenderException`,
     fetcher
@@ -17,27 +46,7 @@ function Example() {
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-  return (
-    // <pre>{JSON.stringify(data.exclusions[0].path, null, 2)}</pre>
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Server</Table.HeaderCell>
-          <Table.HeaderCell>Path</Table.HeaderCell>
-          <Table.HeaderCell>Extension</Table.HeaderCell>
-          <Table.HeaderCell>Process Path</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>{data[1].server}</Table.Cell>
-          <Table.Cell>{data[1].exclusions[0].path}</Table.Cell>
-          <Table.Cell>{data[1].exclusions[0].extension}</Table.Cell>
-          <Table.Cell>{data[1].exclusions[0].processPath}</Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table>
-  );
+  return <ExceptionTable rows={data} />;
 }
 
 const App = () => (
@@ -53,18 +62,8 @@ const App = () => (
         </a>
       </>
     }
-    side={
-      <>
-        <a href="/path">
-          <Nav.Item icon={faHome}>Home</Nav.Item>
-        </a>
-        <a href="/path">
-          <Nav.Item icon={faCog}>Settings</Nav.Item>
-        </a>
-      </>
-    }
   >
-    <Example />
+    <DefenderOverview />
   </Nav>
 );
 
